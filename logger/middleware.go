6c3275990efd40ad6dff8f6abfab068d6ed9501b
Valentino Ekaputra <silentrazgriz@gmail.com>
2020-07-14 22:52:50 +0700

    feature: add grpc request and endpoint middleware

--- logger/middleware.go
+++ logger/middleware.go
@@ -3,9 +3,12 @@ package logger
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"reflect"
+	"runtime"
 	"time"
 
 	"github.com/go-kit/kit/endpoint"
@@ -62,6 +65,39 @@ func GrpcMiddleware(logger *zap.Logger) endpoint.Middleware {
 	return func(f endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, in interface{}) (out interface{}, err error) {
 			ctx = NewLoggerContext(ctx, logger)
+			return f(ctx, in)
+		}
+	}
+}
+
+func GrpcEndpointMiddleware() endpoint.Middleware {
+	return func(f endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, in interface{}) (out interface{}, err error) {
+			logger := GetLogger(ctx)
+
+			funcName := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+			logger.Info(fmt.Sprintf("begin grpc request: %s", funcName))
+
+			start := time.Now()
+			out, err = f(ctx, in)
+			elapsed := time.Since(start)
+
+			logger.Info(fmt.Sprintf("end grpc request: %s", elapsed))
+			return out, err
+		}
+	}
+}
+
+func GrpcRequestMiddleware() endpoint.Middleware {
+	return func(f endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, in interface{}) (out interface{}, err error) {
+			logger := GetLogger(ctx)
+			body, _ := json.Marshal(in)
+
+			logger.Debug("grpc request payload",
+				zap.String("payload", string(body)),
+			)
+
 			return f(ctx, in)
 		}
 	}
